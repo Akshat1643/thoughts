51603c20a118ac88a7843c9e667439ab83615834
unknown <akshat160403@gmail.com>
2024-10-28 23:15:00 +0530

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose")
 mongoose.connect("mongodb+srv://akshat160403:2dK3LU0lWRpCRYPG@cluster0.8j1lk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 const express = require('express')
+const serverless = require('serverless-http');
 const app = express();
 const userRoute = require('./routes/userRoute')
 app.use('/',userRoute)
@@ -8,4 +9,5 @@ app.listen(3000,()=>{
     console.log("hanji chal raha ha")
 })
 module.exports = app;
+module.exports.handler = serverless(app);
 //2dK3LU0lWRpCRYPG
