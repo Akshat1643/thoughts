129589bff627db95492043ff08d9c02e09a96780
unknown <akshat160403@gmail.com>
2024-10-28 23:27:47 +0530

    first commit

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 const mongoose = require("mongoose")
 mongoose.connect("mongodb+srv://akshat160403:2dK3LU0lWRpCRYPG@cluster0.8j1lk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 const express = require('express')
-const serverless = require('serverless-http');
 const app = express();
 const userRoute = require('./routes/userRoute')
 app.use('/',userRoute)
 app.listen(3000,()=>{
     console.log("hanji chal raha ha")
 })
-module.exports = app;
-module.exports.handler = serverless(app);
 //2dK3LU0lWRpCRYPG
